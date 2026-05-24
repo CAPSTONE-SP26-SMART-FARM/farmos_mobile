2eeb940bee8e536bc2130b0bb76bfa77d002931e
Minh Nhật Mai <minatt2002@gmail.com>
2026-05-24 14:25:26 +0700

    feat: canhbao UI

--- src/hooks/useAlert.ts
+++ src/hooks/useAlert.ts
@@ -3,19 +3,20 @@ import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { queryKeys } from '@/constants/queryKeys'
 import { alertApi } from '@/services/api/alert'
 import { socketService } from '@/services/socket/socketService'
-import type { Alert } from '@/types/alert'
 
 export function useAlertList(page = 1) {
   const queryClient = useQueryClient()
 
   useEffect(() => {
-    function onAlertCreated(_alert: Alert) {
-      queryClient.invalidateQueries({ queryKey: queryKeys.alerts.list() })
+    const refresh = () => {
+      queryClient.refetchQueries({ queryKey: ['alerts', 'list'] })
     }
 
-    socketService.on('alert.created', onAlertCreated)
+    socketService.on('alert.created', refresh)
+    socketService.on('sensor.alert.recovered', refresh)
     return () => {
-      socketService.off('alert.created', onAlertCreated)
+      socketService.off('alert.created', refresh)
+      socketService.off('sensor.alert.recovered', refresh)
     }
   }, [queryClient])
 
