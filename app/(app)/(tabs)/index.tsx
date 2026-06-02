38e570b67bdc1c51d18be5512bb7444a95ca6914
tamnguyendev <th3m1nd05@gmail.com>
2026-06-02 15:28:46 +0700

    Update index.tsx

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react'
-import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, ScrollView, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { router } from 'expo-router'
 import * as WebBrowser from 'expo-web-browser'
